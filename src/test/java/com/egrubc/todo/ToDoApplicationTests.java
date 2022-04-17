3af5ec60f43f3004c0d1e13a1ad4dc0f9bdfffea
ConnorDBurge <connor.d.burge@gmail.com>
2022-04-17 10:34:50 -0700

    created Spring Boot project

---
+++ src/test/java/com/egrubc/todo/ToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egrubc.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
